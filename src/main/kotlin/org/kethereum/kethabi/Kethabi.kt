ed356dd2b0d11ac955449a835d058b8f49c99b6b
ligi <ligi@ligi.de>
2019-11-06 14:25:08 +0900

    Separate task

--- src/main/kotlin/org/kethereum/kethabi/Kethabi.kt
+++ src/main/kotlin/org/kethereum/kethabi/Kethabi.kt
@@ -3,6 +3,7 @@ package org.kethereum.kethabi
 import com.squareup.moshi.Moshi
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.compile.AbstractCompile
 import org.kethereum.abi.EthereumABI
 import org.kethereum.abi_codegen.toKotlinCode
 import java.io.File
@@ -10,14 +11,26 @@ import java.io.File
 class Kethabi : Plugin<Project> {
     override fun apply(target: Project) {
 
-        val outDir = target.buildDir.resolve("generated/kethabi")
-        outDir.deleteRecursively()
-
-        println("generating kethabi code to $outDir")
-
+        val kethabiTask = target.tasks.create("kethabi")
         val sourcePath = "src/main/abi/"
-        processDirectory(target.file(sourcePath), sourcePath, outDir)
 
+        kethabiTask.apply {
+            group = "Build"
+            description = "Generate Kotlin code bindings from ABIs in $sourcePath"
+            inputs.dir(target.file(sourcePath))
+            doLast {
+                val outDir = target.buildDir.resolve("generated/kethabi")
+                outDir.deleteRecursively()
+
+                println("generating kethabi code to $outDir")
+
+                processDirectory(target.file(sourcePath), sourcePath, outDir)
+            }
+        }
+
+        target.tasks.withType(AbstractCompile::class.java).forEach {
+            it.dependsOn(kethabiTask)
+        }
     }
 
     private fun processDirectory(target: File, sourcePath: String, outDir: File) {
