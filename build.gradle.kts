cd1dd10d42668729079f34d62dce5c8cf66bc8af
ligi <ligi@ligi.de>
2020-08-21 04:29:44 +0200

    Use Kethereum 0.83.0
    for full Kotlin 1.4.0 migration (Kethereum 0.83.0 is compiled against 1.4.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // https://github.com/komputing/kethabi/issues/6
-val kethereumVersion = "0.82.6"
+val kethereumVersion = "0.83.0"
 
 apply {
     from("https://raw.githubusercontent.com/ligi/gradle-common/master/versions_plugin_stable_only.gradle")
