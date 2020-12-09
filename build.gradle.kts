841f2618e6cc4f6d68dcb30c3851d232472eca4f
ligi <ligi@ligi.de>
2020-12-09 09:38:20 +0100

    Use moshi 1.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation(localGroovy())
     implementation("com.squareup:kotlinpoet:1.7.2")
 
-    implementation("com.squareup.moshi:moshi-kotlin:1.9.3")
+    implementation("com.squareup.moshi:moshi-kotlin:1.11.0")
 
     implementation("com.github.komputing.kethereum:abi:$kethereumVersion")
     implementation("com.github.komputing.kethereum:abi_codegen:$kethereumVersion")
