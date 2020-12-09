d965c3ff146ec360796cbccb7afced9087037721
ligi <ligi@ligi.de>
2020-12-09 09:37:18 +0100

    Use jupiter 5.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,8 +39,8 @@ dependencies {
     implementation("com.github.komputing.kethereum:abi_codegen:$kethereumVersion")
 
     testImplementation("org.assertj:assertj-core:3.16.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("io.mockk:mockk:1.10.3")
