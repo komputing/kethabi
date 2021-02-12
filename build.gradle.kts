3381853e6b165ca9a9b806ee400d7cadbe350ef4
ligi <ligi@ligi.de>
2021-02-12 11:35:47 +0100

    Use mockk 1.10.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,6 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("io.mockk:mockk:1.10.3")
+    testImplementation("io.mockk:mockk:1.10.5")
 
 }
