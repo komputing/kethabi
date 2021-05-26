5af885f94029f38a5806a6c62a419a270c527917
ligi <ligi@ligi.de>
2021-05-26 05:07:18 +0200

    Use Kotlin 1.5.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
     maven("https://www.jitpack.io")
 }
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.4.30"
+    id("org.jetbrains.kotlin.jvm") version "1.5.10"
     maven
 }
 
