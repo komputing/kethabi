1c022f9567274da11ab5dc6b4907dfcbe9d70d30
ligi <ligi@ligi.de>
2025-01-19 20:41:25 +0100

    Use kotlin 2.0.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
     maven("https://www.jitpack.io")
 }
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.5.20"
+    id("org.jetbrains.kotlin.jvm") version "2.0.21"
     id("maven-publish")
 }
 
