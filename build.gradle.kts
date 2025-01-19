2082238ed0663c30025defb4807e9c8ba332f5dd
ligi <ligi@ligi.de>
2025-01-19 23:05:15 +0100

    Use Ketherum 0.86.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // https://github.com/komputing/kethabi/issues/6
-val kethereumVersion = "0.84.2"
+val kethereumVersion = "0.86.0"
 
 apply {
     from("https://raw.githubusercontent.com/ligi/gradle-common/master/versions_plugin_stable_only.gradle")
